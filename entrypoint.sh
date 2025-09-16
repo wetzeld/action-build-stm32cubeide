5e1bb7a5145050f5b9eb1e031dab4d005d9a4da4
Dominik Wetzel <dominik@wetzel.one>
2025-09-16 14:39:32 +0200

    Disable the indexer
    By default, the indexer runs also in headless mode. Disabling it, can significantly improve build times for larger projects.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -importAll "$1"
-headless-build.sh -data /tmp/stm-workspace -build "$2"
+stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -no-indexer -data /tmp/stm-workspace -importAll "$1"
+headless-build.sh -no-indexer -data /tmp/stm-workspace -build "$2"
